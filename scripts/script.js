925ffaae884dbf364758229270ce2847625c4206
willian massao <brcraftgames@gmail.com>
2022-07-24 15:29:14 -0300

    mais parallax

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,9 @@ function hab(id, state) {
 
     state == true? p[0].hidden = false: p[0].hidden = true;
 }
-
+    var layer5 = document.getElementById("layer5");
+    var layer4e = document.getElementById("layer4e");
+    var layer4d = document.getElementById("layer4d");
     var layer3 = document.getElementById("layer3");
     var layer2 = document.getElementById("layer2");
     var layer1 = document.getElementById("layer1");
@@ -26,6 +28,10 @@ function hab(id, state) {
     addEventListener('scroll', function(){
         var value = window.scrollY;
 
+        layer5.style.left = `${-value * 1}px`;
+        layer5.style.bottom = `${-value * 0.3}px`;
+        layer4e.style.left = `${-value * 0.6}px`;
+        layer4d.style.right = `${-value * 0.6}px`;
         layer3.style.top = `${-value * 0.00}px`;
         layer2.style.top = `${-value * 0.15}px`;
         layer1.style.top = `${-value * 0.50}px`;
